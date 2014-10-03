5643c25675a6d3a5ce9bbe975fd7614e18947e3c
SKART1 <art.enikoff@gmail.com>
2014-10-03 15:53:13 +0400

    De initializing step by step added

--- Lab2/modulePWM.c
+++ Lab2/modulePWM.c
@@ -38,13 +38,21 @@
 /*===============================================================================*/
 
 
-ProgrammContext programmContext;
 
+
+/*********************************************************************************/
+/*							Global variables									 */	
+/*********************************************************************************/
+ProgrammContext programmContext={0,0,0,NULL,0,0};
+/*===============================================================================*/
 
 
 /*********************************************************************************/
 /*									Interrupts									 */	
 /*********************************************************************************/
+ /*
+  *increment current duty cycle (decrement length of HIGH value of LED,increment length of LOW value of LED state
+  */
 static irqreturn_t usrBtn1IRQHandler(int irq,void *dev_id){
 	//TODO: Add duty cycle decremnet by 10%
     //usrled_val = 1 & ~usrled_val;
@@ -54,6 +62,9 @@ static irqreturn_t usrBtn1IRQHandler(int irq,void *dev_id){
     return( IRQ_HANDLED );
 }
 
+ /*
+  *Decrement current duty cycle (increment length of HIGH value of LED, decrement length of LOW value of LED state
+  */
 static irqreturn_t usrBtn2IRQHandler(int irq, void *dev_id){
    // usrled_val = 1 & ~usrled_val;
    // gpio_set_value(GPIO_USRLED, usrled_val);
@@ -62,12 +73,21 @@ static irqreturn_t usrBtn2IRQHandler(int irq, void *dev_id){
     return( IRQ_HANDLED );
 }
 
+ /*
+  *Invert LED value. Load new value to LOAD register of EPIT depending on value of LED
+  */
 static irqreturn_t irqEPITHandler(int irq, void *dev_id){
 	
 	//TODO add cleaning interupt bit in EPIT status register
 	printk("[INFO]: Timer\n");
 	programmContext.usrled_val = 1 & ~ programmContext.usrled_val;
     gpio_set_value(GPIO_USRLED, programmContext.usrled_val);
+	if(programmContext.usrled_val==0){
+	
+	}
+	else{
+	
+	}
 
 	iowrite32(ioread32(((char *)programmContext.EPITRegistersMapBegin)+4)|0x00000001,((char *)programmContext.EPITRegistersMapBegin)+4);
 	return( IRQ_HANDLED );
@@ -91,13 +111,9 @@ static irqreturn_t irqEPITHandler(int irq, void *dev_id){
   */
 int initEPITForPWMAndStart(){
 	/*Declaring variables*/
-	//Load register value
-	//unsigned long int MODULUS_REGISTER_VALUE_LED_HIGH=0;
-	//unsigned long int MODULUS_REGISTER_VALUE_LED_LOW=0;
-
 	//Control register for EPIT N1
 	unsigned long int EPIT1_EPITCR=0;
-	void *test; 	
+	void *clockControllerMemoryMapping; 	
 	/*-------------------------------------------------*/
 
 
@@ -108,8 +124,8 @@ int initEPITForPWMAndStart(){
 		programmContext.modulusRegisterValueLEDLow=((LOW_FREQUENCY_CLOCK_SOURCE_HZ/PRESCALER)/REQUIRED_FREQUENCY_HZ)*(1-START_DUTY_CYCLE);
 	}
 	else{	
-		printk("[ERROR]: The of  LOW_FREQUENCY_CLOCK_SOURCE_HZ, PRESCALER, REQUIRED_FREQUENCY_HZ results in value for MODULUS register \n");
-		return -1;
+		printk("[ERROR]: LOW_FREQUENCY_CLOCK_SOURCE_HZ, PRESCALER, REQUIRED_FREQUENCY_HZ values gives result too LOW for correct PWM work \n");
+		goto ERROR_STEP0;
 	};
 
 	//EPIT control register
@@ -139,65 +155,78 @@ int initEPITForPWMAndStart(){
 	/*-------------------------------------------------*/
 
 
-	/*Write values to device*/	
+	/*Write settings to device*/	
 	//EPIT settings
 	if(request_mem_region(BASE_ADRESS,REGISTERS_TOTAL_LENGTH, "EPIT")==NULL){
 		printk("[ERROR]: request_mem_region error\n");
-		return -1;
+		goto ERROR_STEP1;
 	}	
+	//map physical memory to virtual memory of module
 	programmContext.EPITRegistersMapBegin=ioremap(BASE_ADRESS,  REGISTERS_TOTAL_LENGTH);	
-printk("%X\n", ioread32(((char *)programmContext.EPITRegistersMapBegin)));
-printk("%X\n", ioread32(((char *)programmContext.EPITRegistersMapBegin)+4));
-printk("%X\n", ioread32(((char *)programmContext.EPITRegistersMapBegin)+8));
-printk("%X\n", ioread32(((char *)programmContext.EPITRegistersMapBegin)+12));
 
+	//Write EPIT config register
+	//TODO add offsets of registers in EPIT.h
 	iowrite32(EPIT1_EPITCR, programmContext.EPITRegistersMapBegin);
 
-	//Compare register is zero -because of algorythm
+	//Compare register is set to zero: because of algorythm
 	iowrite32(0L, ((char *)programmContext.EPITRegistersMapBegin)+12);
 
-	//Compare register is zero -because of algorythm
+	//Start value - let it be LED high length (because LED is also started at that moment)
 	iowrite32(programmContext.modulusRegisterValueLEDHigh, ((char *)programmContext.EPITRegistersMapBegin)+8);
 	/*-------------------------------------------------*/
 
 
-	/* IRQ */
-	//Enable IRQ
-	enable_irq(EPIT_IRQ_NUMBER);
+	/* IRQ */	
 	//request irq
 	if(request_irq(EPIT_IRQ_NUMBER,(*irqEPITHandler),IRQF_TRIGGER_FALLING, MODULE_NAME,NULL)!=0){
 		printk(KERN_INFO "[ERROR]: %s: EPIT 2 request_irq\n", MODULE_NAME);
-		return -1;
+		goto ERROR_STEP2;
 	};	
+	//Enable IRQ
+	//TODO check if it is needed to disable IRQ...
+	enable_irq(EPIT_IRQ_NUMBER);
+
+
 
 	/*Switch on CLOCKING*/
 	//map memory
 	if(request_mem_region(0x53FD4070,4, "CLOCK")==NULL){
 		printk("[ERROR]: request_mem_region clock error\n");
-		return -1;
+		goto ERROR_STEP3;
 	}	
-	test=ioremap(0x53FD4070, 4);
+	clockControllerMemoryMapping=ioremap(0x53FD4070, 4);
 	//Clock settings
 	iowrite32(ioread32(test)|0x0000000C,test);
-
 	//unmap memory
 	iounmap(test);
 	release_mem_region(0x53FD4070, 4);
 
-
-printk("%X\n", ioread32(((char *)programmContext.EPITRegistersMapBegin)+16));
-printk("%X\n", ioread32(((char *)programmContext.EPITRegistersMapBegin)+16));
-printk("%X\n", ioread32(((char *)programmContext.EPITRegistersMapBegin)+16));
-printk("%X\n", ioread32(((char *)programmContext.EPITRegistersMapBegin)+16));
-printk("%X\n", ioread32(((char *)programmContext.EPITRegistersMapBegin)+16));
 	return 0;
+
+
+	
+ERROR_STEP3:
+	disable_irq(EPIT_IRQ_NUMBER);
+	free_irq(EPIT_IRQ_NUMBER,MODULE_NAME); 
+ERROR_STEP2:
+ERROR_STEP1:
+ERROR_STEP0:
+	return -1;
 }
  /*-------------------------------------------------------------------------------*/
 
  /*
-  *
+  *Uninitialize EPIT: 
+  *1). Stop timer 
+  *2). Disable IRQ if necessary 
+  *3). Release(free) IRQ  
+  *4). Unmap memory region
+  *5). Release memory region
   */
 void deInitEPIT(){
+	//TODO: Stop timer
+	//TODO check if it is needed to disable IRQ...
+
 	free_irq(EPIT_IRQ_NUMBER,MODULE_NAME); 
 	disable_irq(EPIT_IRQ_NUMBER);
 	iounmap(programmContext.EPITRegistersMapBegin);
@@ -215,7 +244,7 @@ int initLED(unsigned int portLEDNumber){
 	//and http://lwn.net/Articles/532714/
 	if(gpio_is_valid(portLEDNumber)==0){
 		printk(KERN_INFO "[ERROR]: %s: initLED gpio_is_valid\n", MODULE_NAME);
-		return -1;
+		goto ERROR_STEP0;
 	}
 
 
@@ -229,7 +258,7 @@ int initLED(unsigned int portLEDNumber){
 	*/
 	if(gpio_request(portLEDNumber, MODULE_NAME)!=0){
 		printk(KERN_INFO "[ERROR]: %s: initLED gpio_request\n", MODULE_NAME);
-		goto ERROR;
+		goto ERROR_STEP1;
 	}
 
 	
@@ -241,23 +270,27 @@ int initLED(unsigned int portLEDNumber){
 	*/
 	if(gpio_direction_output(portLEDNumber,programmContext.usrled_val)!=0){
 		printk(KERN_INFO "[ERROR]: %s: initLED gpio_direction_output\n", MODULE_NAME);
-		goto ERROR;
+		goto ERROR_STEP2;
 	}
 	return 0;
 
 
-ERROR:
+ERROR_STEP2:
+ERROR_STEP1:
 	//We have requested gpio - so we should free it even if it were error
-	gpio_free(portLEDNumber);	
+	gpio_free(portLEDNumber);
+ERROR_STEP0:
 	return -1;
 };
+ /*-------------------------------------------------------------------------------*/
 
  /*
+  *	We need just to say system we no longer need this GPIO port
   */
 void deInitLed(unsigned long int portLEDNumber){
 	gpio_free(portLEDNumber);
 }
-/*===============================================================================*/
+
 
  /*
   * Initialize USERBUTTONS - first checks if the ports are correct, after that setting pins (ports) as inputs.
@@ -269,32 +302,32 @@ int initButtons(unsigned long int  portButton1Number, unsigned long int  portBut
 	//and http://lwn.net/Articles/532714/
 	if(gpio_is_valid(portButton1Number)==0){
 		printk(KERN_INFO "[ERROR]: %s: initButtons 1 gpio_is_valid\n", MODULE_NAME);
-		return -1;
+		goto ERROR_STEP0_1;;
 	};	
 	if(gpio_is_valid(portButton2Number)==0){
 		printk(KERN_INFO "[ERROR]: %s: initButtons 2 gpio_is_valid\n", MODULE_NAME);
-		return -1;
+		goto ERROR_STEP0_2;
 	};	
 
 	//Allocate gpio (see description above)
 	if(gpio_request(portButton1Number, MODULE_NAME )!=0){
 		printk(KERN_INFO "[ERROR]: %s: initButtons 1 gpio_request\n", MODULE_NAME);
-		goto ERROR;
+		goto ERROR_STEP1_1;
 	};
 	if(gpio_request(portButton2Number, "TESTMY" )!=0){
 		printk(KERN_INFO "[ERROR]: %s: initButtons 2 gpio_request\n", MODULE_NAME);
-		goto ERROR;
+		goto ERROR_STEP1_2;
 	};
 
 
 	//Set direction as input
 	if(gpio_direction_input(portButton1Number)!=0){
 		printk(KERN_INFO "[ERROR]: %s: initButtons 1 gpio_direction_input\n", MODULE_NAME);
-		goto ERROR;
+		goto ERROR_STEP2_1;
 	};
 	if(gpio_direction_input(portButton2Number)!=0){
 		printk(KERN_INFO "[ERROR]: %s: initButtons 2 gpio_direction_input\n", MODULE_NAME);
-		goto ERROR;
+		goto ERROR_STEP2_2;
 	};
 
 
@@ -308,79 +341,99 @@ int initButtons(unsigned long int  portButton1Number, unsigned long int  portBut
 	*/
 	if((programmContext.usrBtn1IRQNumber = gpio_to_irq(portButton1Number))<0){
 		printk(KERN_INFO "[ERROR]: %s: initButtons 1 gpio_to_irq\n", MODULE_NAME);
-		goto ERROR;
+		goto ERROR_STEP3_1;
 	};
 	if((programmContext.usrBtn2IRQNumber = gpio_to_irq(portButton2Number))<0){
 		printk(KERN_INFO "[ERROR]: %s: initButtons 2 gpio_to_irq\n", MODULE_NAME);
-		goto ERROR;
+		goto ERROR_STEP3_2;
 	};
 
 	//Set irq handler to function  usrBtn1IRQHandler
 	if(request_irq(programmContext.usrBtn1IRQNumber,(*usrBtn1IRQHandler),IRQF_TRIGGER_FALLING,MODULE_NAME,NULL)!=0){
 		printk(KERN_INFO "[ERROR]: %s: initButtons 1 request_irq\n", MODULE_NAME);
-		goto ERROR;
+		goto ERROR_STEP4_1;
 	};
 	if(request_irq(programmContext.usrBtn2IRQNumber,(*usrBtn2IRQHandler),IRQF_TRIGGER_FALLING,MODULE_NAME,NULL)!=0){
 		printk(KERN_INFO "[ERROR]: %s: initButtons 2 request_irq\n", MODULE_NAME);
-		goto ERROR;
+		goto ERROR_STEP4_2;
 	};		
 	return 0;
 
-ERROR:
-	//We have requested gpio - so we should free it even if it were error
-    free_irq(programmContext.usrBtn1IRQNumber, NULL);
-    free_irq(programmContext.usrBtn2IRQNumber, NULL);
-	gpio_free(portButton1Number);	
+ERROR_STEP4_2:
+	free_irq(programmContext.usrBtn1IRQNumber, NULL);
+ERROR_STEP4_1;
+ERROR_STEP3_1:  	
+ERROR_STEP3_2:  
+ERROR_STEP2_2:   
+ERROR_STEP2_1:   
 	gpio_free(portButton2Number);
+ERROR_STEP1_2:	
+	gpio_free(portButton1Number);	
+ERROR_STEP1_1:	
+ERROR_STEP0_2:
+ERROR_STEP0_1:
 	return -1;	
 };
-/*===============================================================================*/
+ /*-------------------------------------------------------------------------------*/
 
-/*
-*/
+ /*
+  *Say to system - no longer our IRQ handlers and no longer need this GPIO
+  */
 void deInitButtons(unsigned long int  portButton1Number, unsigned long int portButton2Number){
 	//delete IRQ`s	
     free_irq(programmContext.usrBtn1IRQNumber, NULL);
     free_irq(programmContext.usrBtn2IRQNumber, NULL);
-
+	//Say to kernel "We no longer use gpio`s"
 	gpio_free(portButton1Number);	
 	gpio_free(portButton2Number);		
 };
-/*===============================================================================*/
+ /*-------------------------------------------------------------------------------*/
 
 
-
+/*
+ * 
+ */
 static int __init init_routine(void){
 	printk( KERN_INFO "%s: initialization.\n", MODULE_NAME);
-	programmContext.usrled_val=0;
+	programmContext.usrled_val=1;
+
 	if(initLED(GPIO_USRLED)!=0){
-		goto ERROR;
+		goto ERROR_STEP0;
 	}
 
 	if(initButtons(GPIO_USRBTN1, GPIO_USRBTN2)!=0){
-		goto ERROR;
+		goto ERROR_STEP1;
 	}
 	if(initEPITForPWMAndStart()!=0){
-		goto ERROR;
+		goto ERROR_STEP2;
 	}
 	printk( KERN_INFO "[INFO] %s: initialization finished. Module has been started\n", MODULE_NAME);
 
 	return 0;
 
-ERROR:
-	deInitButtons(GPIO_USRBTN1, GPIO_USRBTN2);
-	deInitLed(GPIO_USRLED);
-	deInitEPIT();
+ERROR_STEP2:	
+ERROR_STEP1:	
+ERROR_STEP0:	
 	return -1;
 }
+ /*-------------------------------------------------------------------------------*/
 
+ /*
+  * 
+  */
 static void __exit exit_routine(void){
+	deInitEPIT();
 	deInitButtons(GPIO_USRBTN1, GPIO_USRBTN2);
 	deInitLed(GPIO_USRLED);
-	deInitEPIT();
-
 	printk(KERN_INFO "[INFO]: %s: stopped.\n", MODULE_NAME);
 }
+ /*-------------------------------------------------------------------------------*/
+/*===============================================================================*/
 
+
+/*********************************************************************************/
+/*									Init and Deinit module						 */	
+/*********************************************************************************/
 module_init(init_routine);
 module_exit(exit_routine);
+/*===============================================================================*/

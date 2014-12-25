d76d74e94c1b9091a193c88711f4d1211c7bc728
unknown <art.enikoff@gmail.com>
2014-12-25 15:27:51 +0300

    change lab2

--- STM32Labs/Lab2/board_config.h
+++ STM32Labs/Lab2/board_config.h
@@ -6,17 +6,6 @@
   #define LED_PORT      GPIOD
   #define LED_PIN       GPIO_Pin_12
 
-  #define PIN1_RCC      RCC_AHB1Periph_GPIOD
-  #define PIN1_PORT     GPIOD
-  #define PIN1_PIN      GPIO_Pin_2
-
-  #define PIN2_RCC      RCC_AHB1Periph_GPIOD
-  #define PIN2_PORT     GPIOD
-  #define PIN2_PIN      GPIO_Pin_6
-
-  #define BUTTON_RCC      RCC_AHB1Periph_GPIOA
-  #define BUTTON_PORT     GPIOA
-  #define BUTTON_PIN      GPIO_Pin_0
 
 #endif
 
@@ -25,17 +14,6 @@
   #define LED_PORT      GPIOF
   #define LED_PIN       GPIO_Pin_6
 
-  #define PIN1_RCC      RCC_AHB1Periph_GPIOF
-  #define PIN1_PORT     GPIOF
-  #define PIN1_PIN      GPIO_Pin_8
-
-  #define PIN2_RCC      RCC_AHB1Periph_GPIOF
-  #define PIN2_PORT     GPIOF
-  #define PIN2_PIN      GPIO_Pin_6
-
-  #define BUTTON_RCC      RCC_AHB1Periph_GPIOG
-  #define BUTTON_PORT     GPIOG
-  #define BUTTON_PIN      GPIO_Pin_6
 
 #endif
 
--- STM32Labs/Lab2/main.c
+++ STM32Labs/Lab2/main.c
@@ -3,131 +3,40 @@
 #define _STM_IAR_BOARD_
 #include "board_config.h"
 
-
-void Mode0(void);
-void Mode1(void);
-
 void MCO_Enable(void);
 int main()
 {
-	uint8_t	mode = 0;
-	GPIO_InitTypeDef	GPIO_InitStruct;
-	
-	// ----------------------------------------------------
-	//  ENABLE RCC
-	// ----------------------------------------------------
-	RCC_AHB1PeriphClockCmd(PIN1_RCC, ENABLE);
-	RCC_AHB1PeriphClockCmd(PIN2_RCC, ENABLE);
-        RCC_AHB1PeriphClockCmd(BUTTON_RCC, ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
-	
-	DBGMCU_APB1PeriphConfig(DBGMCU_TIM2_STOP, ENABLE);
-	DBGMCU_APB1PeriphConfig(DBGMCU_TIM3_STOP, ENABLE);
-	
-	// ----------------------------------------------------
-	//  INIT GPIO
-	// ----------------------------------------------------
-	GPIO_InitStruct.GPIO_Pin = PIN1_PIN;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(PIN1_PORT, &GPIO_InitStruct);
-        
-        GPIO_InitStruct.GPIO_Pin = PIN2_PIN;
-        GPIO_Init(PIN2_PORT, &GPIO_InitStruct);
-        
-	GPIO_InitStruct.GPIO_Pin = BUTTON_PIN;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
-	GPIO_Init(BUTTON_PORT, &GPIO_InitStruct);
-	
-	Mode0();
-	
-	while(1){
-		if(GPIO_ReadInputDataBit(BUTTON_PORT, BUTTON_PIN) == Bit_RESET){
-			if (mode == 0){
-				Mode1();
-				mode = 1;
-			}
-			else{
-				Mode0();
-				mode = 0;
-			}
-			while(GPIO_ReadInputDataBit(BUTTON_PORT, BUTTON_PIN) == Bit_RESET);
-		}
-	}
-}
-void TIM2_IRQHandler(void){
-	uint32_t	i = 0;
-	
-	TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
-	TIM_ClearFlag(TIM2, TIM_FLAG_Update);
-	
-	GPIO_SetBits(PIN1_PORT, PIN1_PIN);
-	while(i++ < 1000){}
-	GPIO_ResetBits(PIN1_PORT, PIN1_PIN);
-	
-}
-void TIM3_IRQHandler(void){
+  GPIO_InitTypeDef	GPIO_InitStruct;
+  
+  RCC_AHB1PeriphClockCmd(LED_RCC, ENABLE);
 
-	TIM_ClearITPendingBit(TIM3,TIM_IT_Update);
-	TIM_ClearFlag(TIM3, TIM_FLAG_Update);
-	
-	GPIO_ToggleBits(PIN2_PORT, PIN2_PIN);
-}
-void Mode0(void){
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseStruct;
-	NVIC_InitTypeDef		NVIC_InitStruct;
-	
-	// ----------------------------------------------------
-	//  INIT TIMER 2
-	// ----------------------------------------------------
-	TIM_TimeBaseStructInit(&TIM_TimeBaseStruct);
-	TIM_TimeBaseStruct.TIM_CounterMode = TIM_CounterMode_Up;
-	TIM_TimeBaseStruct.TIM_Prescaler 	= 0;
-	TIM_TimeBaseStruct.TIM_Period		= 40000;
-	TIM_TimeBaseStruct.TIM_ClockDivision = 0x00;
-	
-	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStruct);
-	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
-	
-	// ----------------------------------------------------
-	//  INIT TIMER 3
-	// ----------------------------------------------------
-	TIM_TimeBaseStruct.TIM_Period		= 100;
-	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStruct);
-	TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
-	
-	// ----------------------------------------------------
-	//  INIT NVIC
-	// ----------------------------------------------------
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+  GPIO_InitStruct.GPIO_Pin = LED_PIN;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
 
-	NVIC_InitStruct.NVIC_IRQChannel = TIM2_IRQn;
-	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority	= 0;
-	NVIC_InitStruct.NVIC_IRQChannelSubPriority			= 0;
-	NVIC_InitStruct.NVIC_IRQChannelCmd					= ENABLE;
-	NVIC_Init(&NVIC_InitStruct);
-	
-	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority	= 0;
-	NVIC_InitStruct.NVIC_IRQChannel = TIM3_IRQn;
-	NVIC_Init(&NVIC_InitStruct);
-	
-	TIM_Cmd(TIM2, ENABLE);
-	TIM_Cmd(TIM3, ENABLE);
+  GPIO_Init(LED_PORT, &GPIO_InitStruct);
+
+  MCO_Enable();
+  
+  while(1){
+    GPIO_ToggleBits(LED_PORT, LED_PIN);
+  }
 }
 
-void Mode1(void){
-	NVIC_InitTypeDef		NVIC_InitStruct;
-	
-	NVIC_InitStruct.NVIC_IRQChannel = TIM2_IRQn;
-	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority	= 1;
-	NVIC_InitStruct.NVIC_IRQChannelSubPriority			= 0;
-	NVIC_InitStruct.NVIC_IRQChannelCmd					= ENABLE;
-	NVIC_Init(&NVIC_InitStruct);
-	
-	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority	= 0;
-	NVIC_InitStruct.NVIC_IRQChannel = TIM3_IRQn;
-	NVIC_Init(&NVIC_InitStruct);
+void MCO_Enable(void){
+  // GPIO Init
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+  
+  GPIO_InitTypeDef		GPIO_InitStruct;
+
+  GPIO_InitStruct.GPIO_Pin 	= GPIO_Pin_8;
+  GPIO_InitStruct.GPIO_Mode 	= GPIO_Mode_AF;
+  GPIO_InitStruct.GPIO_Speed 	= GPIO_Speed_100MHz;
+  GPIO_InitStruct.GPIO_OType 	= GPIO_OType_PP;
+  GPIO_InitStruct.GPIO_PuPd	= GPIO_PuPd_UP;
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  RCC_MCO1Config(RCC_MCO1Source_HSE, RCC_MCO1Div_5);
 }
